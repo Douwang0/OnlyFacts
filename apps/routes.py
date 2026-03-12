46b285df03907b828fd0f9061c3e282f3cee6c48
Douwang <giulian.lapin@gmail.com>
2026-03-12 21:12:49 +0100

    Conditions et Boucles pour exemples

--- apps/routes.py
+++ apps/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from . import app # On utilise . pour utiliser le app deja import
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    utilisateur = {'nom': 'Giulian'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ] # Dico de test, pas d'anglais dans la prod
+    return render_template('index.html', titre='Acceuil', utilisateur=utilisateur, posts=posts)
