c589f02e7602671cc44d9575bec51b4437da9b6f
Douwang <giulian.lapin@gmail.com>
2026-05-13 08:54:47 +0200

    Bouton de upvote fini

--- apps/data.py
+++
@@ -1,7 +0,0 @@
-db_test_votes = []
-
-def utilisateur_a_vote(id_utilisateur, id_post):
-    for vote in db_test_votes:
-        if vote["user_id"] == id_utilisateur and vote["post_id"] == id_post:
-            return True
-    return False
