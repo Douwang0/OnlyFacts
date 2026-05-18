e66cddfec7e0e22324bc0b516a5ed421d516113c
Douwang <giulian.lapin@gmail.com>
2026-05-18 16:49:06 +0200

    Database de text pour mr.Mazza

--- apps/auth.py
+++ apps/auth.py
@@ -3,7 +3,7 @@ from apps.db import get_utilisateur_par_info, ajouter_utilisateur
 
 def recherche_utilisateur(nom,prenom):
     """Prend une liste d'utilisateur par nom prenom et renvoie soit un seul utilisateur soit None si aucun trouvé soit "Plusieurs" si il en existe plusieurs dans la database """
-    u = get_utilisateur_par_info(nom,prenom)
+    u = get_utilisateur_par_info(prenom,nom)
     if u == []:
         return None
     elif len(u) > 1:
@@ -34,5 +34,5 @@ def set_utilisateur(nom,prenom,mdp):
     elif len(mdp) < 7: # 8+ pour un mot de passe robuste
         return "Mdp trop court"
     else:
-        ajouter_utilisateur(nom,prenom,crypter(mdp))
+        ajouter_utilisateur(prenom,nom,crypter(mdp))
         return False
